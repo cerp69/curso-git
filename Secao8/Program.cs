e7bfe66395a844b4e4a8d372583d9c9a58af0c83
Carlos Rodrigues <cerp69@gmail.com>
2019-07-06 16:08:45 -0300

    Acrescentada mensagem de Bom dia

--- Secao8/Program.cs
+++ Secao8/Program.cs
@@ -7,6 +7,20 @@ namespace Secao8
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string mens;
+            if (DateTime.Now.Hour < 12)
+            {
+                mens = "Bom dia!";
+            }
+            else if (DateTime.Now.Hour < 18)
+            {
+                mens = "Boa tarde!";
+            }
+            else
+            {
+                mens = "Boa noite!";
+            }
+            Console.WriteLine(mens);
         }
     }
 }
